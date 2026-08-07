4368ca83351670e5f1abfd5e080479c3bbca0b2a
dieferben-hash <dieferben@gmail.com>
2026-08-07 18:42:10 -0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "tercer-tiempo-clave-secreta-cambiar")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "instance", "tercer_tiempo.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
